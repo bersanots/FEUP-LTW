52485fae6954bddbb04cedcfba050c1dbc6f4677
Bernardo Santos <bernardo.santos10@gmail.com>
2018-12-07 18:40:14 +0000

    Database updated with Like and Dislike

--- askMIEIC/actions/profile_action.php
+++ askMIEIC/actions/profile_action.php
@@ -4,18 +4,18 @@
 
   $user_id = getUserID();
   $old_password = getUser($user_id)['password'];
-  if( $old_password == $POST['old_password']){
+  if($old_password == $POST['old_password']){
     if($POST['new_password'] == $POST['confirm_password']){
-    changePassword($user_id, $_POST['new_password']);
-    changeEmail($user_id, $_POST['email']);
+      changePassword($user_id, $_POST['new_password']);
+      changeEmail($user_id, $_POST['email']);
     } else {
-      $_SESSION['error'] = 'New password did not match the confirmation';
-      header("Location:".$_SERVER['HTTP_REFERER']."");
+        $_SESSION['error'] = 'New password did not match the confirmation';
+        header("Location:".$_SERVER['HTTP_REFERER']."");
+    }
   } else {
 	  $_SESSION['error'] = 'Old password is incorrect';
 	  header("Location:".$_SERVER['HTTP_REFERER']."");
   }
-}
 
   header("Location: ../pages/main.php");
 
