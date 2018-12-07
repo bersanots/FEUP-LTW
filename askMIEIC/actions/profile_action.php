ed3b7bad288e4ad509dc8104e567af031309d89a
Bernardo Santos <bernardo.santos10@gmail.com>
2018-12-07 23:41:10 +0000

    Edit profile working

--- askMIEIC/actions/profile_action.php
+++ askMIEIC/actions/profile_action.php
@@ -4,10 +4,11 @@
 
   $user_id = getUserID();
   $old_password = getUser($user_id)['password'];
-  if($old_password == $POST['old_password']){
-    if($POST['new_password'] == $POST['confirm_password']){
+  if($old_password == hash('sha256', $_POST['old_password'])){
+    if($_POST['new_password'] == $_POST['confirm_new_password']){
       changePassword($user_id, $_POST['new_password']);
       changeEmail($user_id, $_POST['email']);
+      header("Location: ../pages/main.php");
     } else {
         $_SESSION['error'] = 'New password did not match the confirmation';
         header("Location:".$_SERVER['HTTP_REFERER']."");
@@ -16,7 +17,5 @@
 	  $_SESSION['error'] = 'Old password is incorrect';
 	  header("Location:".$_SERVER['HTTP_REFERER']."");
   }
-
-  header("Location: ../pages/main.php");
 
 ?>
--- askMIEIC/pages/edit_profile.php
+++ askMIEIC/pages/edit_profile.php
@@ -4,11 +4,11 @@
 
   draw_header();
   ?>
-  <section id="edit_profile">
+   <section id="edit_profile">
       <h1>Edit your profile</h1>
       <form action="../actions/profile_action.php" method="post">
         <label>
-          Email <input type="text" name="email" required="required">
+          Email <input type="email" name="email" required="required">
         </label>
         <label>
           Old Password <input type="password" name="old_password" required="required">
@@ -17,9 +17,9 @@
           New Password <input type="password" name="new_password" required="required">
         </label>
         <label>
-          Confirm the new password <input type="password" name="confirm_new_password" required="required">
+          Confirm the new Password <input type="password" name="confirm_new_password" required="required">
         </label>
-        <input type="submit" value="Profile">
+        <input type="submit" value="Save">
       </form>
       <?php if(isset($_SESSION['error'])) echo htmlentities($_SESSION['error']); unset($_SESSION['error'])?>
     </section>
